6be91901d29f6a155809db005cdae5cee8ad71b5
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-11-04 08:46:05 +0100

    Green operator for 3D elasticity

--- src/scapin.c
+++ src/scapin.c
@@ -62,30 +62,28 @@ double scapin_grop_hooke_nu(ScapinGreenOperator *op) {
   return SCAPIN_GROP_HOOKE_DATA(op)->nu;
 }
 
-/* void green_apply(double *tau, double *k, double mu, double nu, double *out) {
- */
-/*   const size_t dim = 3; */
-/*   const size_t sym = 6; */
-/*   const double k2 = k[0] * k[0] + k[1] * k[1] + k[2] * k[2]; */
-/*   double *tau_dot_k = g_new(double, dim); */
-/*   tau_dot_k[0] = tau[0] * k[0] + M_SQRT1_2 * (tau[5] * k[1] + tau[4] * k[2]);
- */
-/*   tau_dot_k[1] = tau[1] * k[1] + M_SQRT1_2 * (tau[5] * k[0] + tau[3] * k[2]);
- */
-/*   tau_dot_k[2] = tau[2] * k[2] + M_SQRT1_2 * (tau[4] * k[0] + tau[3] * k[1]);
- */
-/*   double k_dot_tau_dot_k = */
-/*       k[0] * tau_dot_k[0] + k[1] * tau_dot_k[1] + k[2] * tau_dot_k[2]; */
-/*   const double const1 = 1. / mu; */
-/*   out[0] = const1 * (k[0] * tau_dot_k[0]); */
-/*   out[1] = const1 * (k[1] * tau_dot_k[1]); */
-/*   out[2] = const1 * (k[2] * tau_dot_k[2]); */
-/*   out[3] = const1 * (M_SQRT1_2 * (k[1] * tau_dot_k[2] + tau_dot_k[1] *
- * k[2])); */
-/*   out[4] = const1 * (M_SQRT1_2 * (k[2] * tau_dot_k[0] + tau_dot_k[2] *
- * k[0])); */
-/*   out[5] = const1 * (M_SQRT1_2 * (k[0] * tau_dot_k[1] + tau_dot_k[0] *
- * k[1])); */
+void scapin_grop_hooke_3d_apply(ScapinGreenOperator *op, double *k, double *tau,
+                                double *out) {
+  double const mu = SCAPIN_GROP_HOOKE_DATA(op)->mu;
+  double const nu = SCAPIN_GROP_HOOKE_DATA(op)->nu;
 
-/*   g_free(tau_dot_k); */
-/* } */
+  double const tau_dot_k[] = {
+      tau[0] * k[0] + M_SQRT1_2 * (tau[5] * k[1] + tau[4] * k[2]),
+      tau[1] * k[1] + M_SQRT1_2 * (tau[5] * k[0] + tau[3] * k[2]),
+      tau[2] * k[2] + M_SQRT1_2 * (tau[4] * k[0] + tau[3] * k[1])};
+  double k_dot_tau_dot_k =
+      k[0] * tau_dot_k[0] + k[1] * tau_dot_k[1] + k[2] * tau_dot_k[2];
+  double const const1 = k_dot_tau_dot_k / (1. - nu);
+  double const k2 = k[0] * k[0] + k[1] * k[1] + k[2] * k[2];
+  double const const2 = 1. / (2. * mu * k2);
+  out[0] = const2 * (k[0] * (tau_dot_k[0] - const1 * k[0]));
+  out[1] = const2 * (k[1] * (tau_dot_k[1] - const1 * k[1]));
+  out[2] = const2 * (k[2] * (tau_dot_k[2] - const1 * k[2]));
+  double const const3 = M_SQRT2 * const2;
+  out[3] = const3 * (0.5 * (k[1] * tau_dot_k[2] + k[2] * tau_dot_k[1]) -
+                     const1 * k[1] * k[2]);
+  out[4] = const3 * (0.5 * (k[2] * tau_dot_k[0] + k[0] * tau_dot_k[2]) -
+                     const1 * k[2] * k[0]);
+  out[5] = const3 * (0.5 * (k[0] * tau_dot_k[1] + k[1] * tau_dot_k[0]) -
+                     const1 * k[0] * k[1]);
+}
