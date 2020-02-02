75361ad0ebfff929a706853cb09887436e54b563
Michael F. Herbst <info@michael-herbst.com>
2020-02-02 20:00:02 +0100

    A few restructurings

--- sandbox/src/Scapin.jl
+++ sandbox/src/Scapin.jl
@@ -1,34 +1,50 @@
 module Scapin
+using StaticArrays
+import Base: size, Matrix
 
-export Hooke, in_size, out_size, greenop_apply!
+export Hooke, HookeBlock, kblock, mul!
 
-const SQRT2 = sqrt(2)
-const SQRT1_2 = 1 / sqrt(2)
-
-struct Hooke{T, DIM}
-    μ::T
-    ν::T
+struct Hooke
+    μ
+    ν
 end
 
-function in_size(::Hooke{T, 2}) where {T}
-    return 3
+struct HookeBlock{T, DIM}
+    hooke::Hooke
+    k::SVector{DIM, T}
 end
 
-function out_size(::Hooke{T, 2}) where {T}
-    return 3
+kblock(hooke::Hooke, k::SVector{DIM, T}) where {DIM, T} = HookeBlock{T, DIM}(hooke, k)
+
+size(::HookeBlock{T, 2}) where T = (3, 3)
+
+function Matrix(block::HookeBlock{T, 2}) where T
+    n, m = size(block)
+    mat = zeros(T, n, m)
+    τ = zeros(T, m)
+    for i in 1:m
+        τ[i] = one(T)
+        mul!(view(mat, :, i), block, τ)
+        τ[i] = zero(T)
+    end
+    mat
 end
 
-function greenop_apply!(hooke::Hooke{T, 2}, k::AbstractArray{T,1},
-                        τ::AbstractArray{T,1}, out::AbstractArray{T,1}) where {T}
-    k² = k[1]*k[1]+k[2]*k[2]
-    τk₁ = τ[1]*k[1]+SQRT1_2*τ[3]*k[2]
-    τk₂ = τ[2]*k[2]+SQRT1_2*τ[3]*k[1]
-    nτn = (k[1]*τk₁+k[2]*τk₂) / k²
+function mul!(out, block::HookeBlock{T, 2}, τ) where {T}
+    hooke = block.hooke
+    k = block.k
+
+    # I think this is too much micro-optimisation for the stage of your code.
+    # Why do you not first write ti down more readable and then only optimise the
+    # parts the Julia compiler has trouble with?
+    τk₁ = τ[1]*k[1]+τ[3]*k[2] / sqrt(2)
+    τk₂ = τ[2]*k[2]+τ[3]*k[1] / sqrt(2)
+    nτn = (k[1]*τk₁+k[2]*τk₂) / sum(abs2, k)
     const1 = nτn / (1-hooke.ν)
-    const2 = 1 / (2*hooke.μ*k²)
+    const2 = 1 / (2*hooke.μ*sum(abs2, k))
     out[1] = const2*(k[1]*(2*τk₁-const1*k[1]))
     out[2] = const2*(k[2]*(2*τk₂-const1*k[2]))
-    const3 = SQRT2*const2
+    const3 = sqrt(2)*const2
     out[3] = const3*(k[1]*τk₂+k[2]*τk₁-const1*k[1]*k[2])
     return out
 end
