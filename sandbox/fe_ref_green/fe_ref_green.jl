a473b8afbfa3b191843f9b128c3e389a82357a89
Sébastien Brisard <sebastien.brisard@m4x.org>
2020-03-13 08:39:43 +0100

    Implement constructor with some checks on DIM

--- sandbox/fe_ref_green/fe_ref_green.jl
+++ sandbox/fe_ref_green/fe_ref_green.jl
@@ -3,6 +3,12 @@ using StaticArrays
 struct CartesianGrid{DIM}
     L::SVector{DIM,Float64}
     N::SVector{DIM,Int}
+    function CartesianGrid{DIM}(L::SVector{DIM,Float64}, N::SVector{DIM,Int}) where {DIM}
+        if DIM < 2 || DIM > 3
+            throw(DomainError(DIM))
+        end
+        new(L, N)
+    end
 end
 
 function modal_strain_displacement(
@@ -31,6 +37,7 @@ function modal_strain_displacement(
             h⁻¹[3] * c[1] * c[2] * s[3],
         ]
     else
+        # This should never occur
         throw(DomainError(DIM))
     end
 end
@@ -74,6 +81,24 @@ function modal_stiffness(grid::CartesianGrid{DIM}, k::SVector{DIM,Int}) where {D
             ],
         ]
     else
+        # This should never occur
         throw(DomainError(DIM))
     end
 end
+
+L2 = @SVector [0.5, 1.]
+N2 = @SVector [32, 64]
+grid2 = CartesianGrid{2}(L2, N2)
+k2 = @SVector [0, 0]
+B2 = modal_strain_displacement(grid2, k2)
+K2 = modal_stiffness(grid2, k2)
+
+L3 = @SVector [0.5, 1., 2.]
+N3 = @SVector [32, 64, 128]
+grid3 = CartesianGrid{3}(L3, N3)
+B3 = modal_strain_displacement(grid3, k3)
+K3 = modal_stiffness(grid3, k3)
+
+L4 = @SVector [0.5, 1., 2., 4.]
+N4 = @SVector [32, 64, 128, 256]
+grid4 = CartesianGrid{4}(L4, N4)
