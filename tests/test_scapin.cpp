7b053599128c31aa288d35f5d5b04a665b69fbf7
Sébastien Brisard <sbrisard@users.noreply.github.com>
2020-07-07 10:11:34 +0200

    Use newly defined class DirectionGenerator

--- tests/test_scapin.cpp
+++ tests/test_scapin.cpp
@@ -23,8 +23,8 @@ void green_operator_matrix(double *n, double nu, double *out) {
   constexpr size_t const ij2i_3d[] = {0, 1, 2, 1, 2, 0};
   constexpr size_t const ij2j_3d[] = {0, 1, 2, 2, 0, 1};
 
-  auto const & ij2i = DIM == 2 ? ij2i_2d : ij2i_3d;
-  auto const & ij2j = DIM == 2 ? ij2j_2d : ij2j_3d;
+  auto const &ij2i = DIM == 2 ? ij2i_2d : ij2i_3d;
+  auto const &ij2j = DIM == 2 ? ij2j_2d : ij2j_3d;
 
   double *gamma_ijkl = out;
 
@@ -49,32 +49,83 @@ void green_operator_matrix(double *n, double nu, double *out) {
   }
 }
 
-void test_grop_hooke_2d_apply() {
-  constexpr size_t const dim = 2;
-  constexpr size_t const sym = 3;
-  size_t const num_theta = 20;
+template <size_t DIM>
+class DirectionGenerator {
+ private:
+  size_t const num_theta;
+  size_t const num_phi;
+  double const delta_theta;
+  double const delta_phi;
+
+ public:
+  DirectionGenerator(size_t num_theta = 1, size_t num_phi = 1)
+      : num_theta(num_theta),
+        num_phi(num_phi),
+        delta_theta(M_PI / (num_theta - 1.)),
+        delta_phi(2 * M_PI / (double)num_phi) {
+    if ((DIM == 2) && (num_phi != 1)) {
+      throw std::invalid_argument("num_phi must be equal to 1 when DIM = 2");
+    }
+  }
+
+  std::array<double, DIM> as_array(size_t i) const {
+    if constexpr (DIM == 2) {
+      double const theta = delta_theta * i;
+      return {cos(theta), sin(theta)};
+    }
+    if constexpr (DIM == 3) {
+      double const theta = delta_theta * (i / num_phi);
+      double const phi = delta_phi * (i % num_phi);
+      double const sin_theta = sin(theta);
+      return {sin_theta * cos(phi), sin_theta * sin(phi), cos(theta)};
+    }
+  }
+
+  class DirectionIterator {
+   private:
+    size_t current;
+    const DirectionGenerator &generator;
+
+   public:
+    DirectionIterator(const DirectionGenerator &generator, size_t current)
+        : generator(generator), current(current){};
+
+    std::array<double, DIM> operator*() { return generator.as_array(current); };
+    bool operator!=(const DirectionIterator &j) const {
+      return current != j.current;
+    }
+    void operator++() { ++current; }
+  };
+
+  DirectionIterator begin() const { return DirectionIterator(*this, 0); }
+  DirectionIterator end() const {
+    return DirectionIterator(*this, num_theta * num_phi);
+  }
+};
+
+template <size_t DIM>
+void test_grop_hooke_apply() {
+  constexpr size_t const sym = (DIM * (DIM + 1)) / 2;
   double const mu = 1.0;
   double const nu = 0.3;
 
-  const size_t num_k_norms = 3;
-  double const k_norm[] = {1.2, 3.4, 5.6};
+  std::array<double, 3> k_norm = {1.2, 3.4, 5.6};
 
-  double const delta_theta = 2 * M_PI / (double)num_theta;
-  double const theta_max = (num_theta - 0.5) * delta_theta;
-
-  Hooke<dim> gamma{mu, nu};
+  Hooke<DIM> gamma{mu, nu};
   double exp[sym * sym];
   double act[sym * sym];
   double tau[sym];
   double eps[sym];
-  double n[dim];
-  for (double theta = 0; theta < theta_max; theta += delta_theta) {
-    n[0] = sin(theta);
-    n[1] = cos(theta);
-    green_operator_matrix<dim>(n, nu, exp);
-    for (size_t i = 0; i < num_k_norms; i++) {
-      double const k_vec[] = {k_norm[i] * n[0], k_norm[i] * n[1],
-                              k_norm[i] * n[2]};
+
+  size_t const num_theta = DIM == 2 ? 20 : 10;
+  size_t const num_phi = DIM == 2 ? 1 : 20;
+  for (auto n : DirectionGenerator<DIM>(num_theta, num_phi)) {
+    green_operator_matrix<DIM>(n.data(), nu, exp);
+    for (size_t i = 0; i < k_norm.size(); i++) {
+      double k_vec[DIM];
+      for (size_t j = 0; j < DIM; j++) {
+        k_vec[j] = k_norm[i] * n[j];
+      }
       for (size_t col = 0; col < sym; col++) {
         tau[col] = 1.;
         gamma.apply(k_vec, tau, eps);
@@ -84,57 +135,7 @@ void test_grop_hooke_2d_apply() {
         tau[col] = 0.;
       }
       for (size_t ijkl = 0; ijkl < sym * sym; ijkl++) {
-        double const err = fabs(act[ijkl] - exp[ijkl]);
         REQUIRE(act[ijkl] == Approx(exp[ijkl]).epsilon(1e-12).margin(1e-12));
-      }
-    }
-  }
-}
-
-void test_grop_hooke_3d_apply() {
-  constexpr size_t const dim = 3;
-  constexpr size_t const sym = 6;
-  size_t const num_theta = 10;
-  size_t const num_phi = 20;
-  double const mu = 1.0;
-  double const nu = 0.3;
-
-  const size_t num_k_norms = 3;
-  double const k_norm[] = {1.2, 3.4, 5.6};
-
-  double const delta_theta = M_PI / (num_theta - 1.);
-  double const theta_max = (num_theta - 0.5) * delta_theta;
-  double const delta_phi = 2 * M_PI / (double)num_phi;
-  double const phi_max = (num_phi - 0.5) * delta_phi;
-
-  Hooke<dim> gamma{mu, nu};
-  double exp[sym * sym];
-  double act[sym * sym];
-  double tau[sym];
-  double eps[sym];
-  double n[dim];
-  for (double theta = 0; theta < theta_max; theta += delta_theta) {
-    double const sin_theta = sin(theta);
-    double const cos_theta = cos(theta);
-    for (double phi = 0; phi < phi_max; phi += delta_phi) {
-      n[0] = sin_theta * cos(phi);
-      n[1] = sin_theta * sin(phi);
-      n[2] = cos_theta;
-      green_operator_matrix<dim>(n, nu, exp);
-      for (size_t i = 0; i < num_k_norms; i++) {
-        double const k_vec[] = {k_norm[i] * n[0], k_norm[i] * n[1],
-                                k_norm[i] * n[2]};
-        for (size_t col = 0; col < sym; col++) {
-          tau[col] = 1.;
-          gamma.apply(k_vec, tau, eps);
-          for (size_t row = 0; row < sym; row++) {
-            act[col + sym * row] = eps[row];
-          }
-          tau[col] = 0.;
-        }
-        for (size_t ijkl = 0; ijkl < sym * sym; ijkl++) {
-          REQUIRE(act[ijkl] == Approx(exp[ijkl]).epsilon(1e-12).margin(1e-12));
-        }
       }
     }
   }
@@ -147,8 +148,8 @@ TEST_CASE("Continuous Green operator") {
       SECTION("3D") { test_data<3>(); }
     }
     SECTION("Apply") {
-      SECTION("2D") { test_grop_hooke_2d_apply(); }
-      SECTION("3D") { test_grop_hooke_3d_apply(); }
+      SECTION("2D") { test_grop_hooke_apply<2>(); }
+      SECTION("3D") { test_grop_hooke_apply<3>(); }
     }
   }
 }
