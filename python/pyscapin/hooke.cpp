8d7e2ccc05f6a44ab54eae77922b7160a5895e53
Sébastien Brisard <sbrisard@users.noreply.github.com>
2020-07-17 10:07:09 +0200

    Define some package variables

--- python/pyscapin/hooke.cpp
+++ python/pyscapin/hooke.cpp
@@ -24,16 +24,18 @@ auto create_binding(py::module m, const char* name) {
       .def_readonly("mu", &Hooke_::mu)
       .def_readonly("nu", &Hooke_::nu)
       .def("__repr__", &Hooke_::repr)
-      .def("apply",
-           [](Hooke_& self, py::array_t<double, py::array::c_style> k,
-              py::array_t<T, py::array::c_style> tau,
-              py::array_t<T, py::array::c_style> out) {
-             self.apply(k.data(), tau.data(), out.mutable_data());
-           }); // TODO: check that stride==1, ndim==1, shape>=isize, etc.
+      .def("apply", [](Hooke_& self, py::array_t<double, py::array::c_style> k,
+                       py::array_t<T, py::array::c_style> tau,
+                       py::array_t<T, py::array::c_style> out) {
+        self.apply(k.data(), tau.data(), out.mutable_data());
+      });  // TODO: check that stride==1, ndim==1, shape>=isize, etc.
 }
 
 PYBIND11_MODULE(hooke, m) {
   m.doc() = "Python bindings to the scapin/hooke.hpp header-only library";
+
+  m.attr("__version__") = py::cast(__SCAPIN_VERSION__);
+  m.attr("__author__") = py::cast(__SCAPIN_AUTHOR__);
 
   create_binding<float64, 2>(m, "HookeFloat64_2D");
   create_binding<float64, 3>(m, "HookeFloat64_3D");
--- python/setup.py
+++ python/setup.py
@@ -11,31 +11,36 @@ def get_metadata(name):
         return f.read().strip()
 
 
-def pybind11_extension(package_name, module_name):
+def pybind11_extension(module_name, metadata):
     return setuptools.Extension(
-        ".".join([package_name, module_name]),
+        ".".join([metadata["name"], module_name]),
         include_dirs=[pybind11.get_include(), SCAPIN_INCLUDE_DIR],
-        sources=[os.path.join(package_name, module_name+".cpp")],
+        sources=[os.path.join(metadata["name"], module_name + ".cpp")],
+        define_macros=[
+            ("__SCAPIN_VERSION__", r'\"' + metadata["version"] + r'\"'),
+            ("__SCAPIN_AUTHOR__", r'\"' + metadata["author"] + r'\"'),
+        ],
     )
 
 
 if __name__ == "__main__":
-    project_name = "pyscapin"
+    metadata = {
+        "name": "pyscapin",
+        "version": get_metadata("version"),
+        "author": get_metadata("author"),
+        "author_email": get_metadata("author_email"),
+        "description": "A framework for FFT-based, full-field numerical simulation of heterogeneous materials",
+        "url": get_metadata("url"),
+    }
 
     with open(os.path.join("..", "README.md"), "r") as f:
-        long_description = f.read()
+        metadata["long_description"] = f.read()
 
-    hooke = pybind11_extension(project_name, "hooke")
+    hooke = pybind11_extension("hooke", metadata)
 
     setuptools.setup(
-        name=project_name,
-        version=get_metadata("version"),
-        author=get_metadata("author"),
-        author_email=get_metadata("author_email"),
-        description="A framework for FFT-based, full-field numerical simulation of heterogeneous materials",
-        long_description=long_description,
         long_description_content_type="text/markdown",
-        url=get_metadata("url"),
         packages=setuptools.find_packages(),
         ext_modules=[hooke],
+        **metadata
     )
