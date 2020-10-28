45025edf8d1491e49f9a5b5a8bf86cfe30e4654c
Sébastien Brisard <sbrisard@users.noreply.github.com>
2020-10-28 06:35:07 +0100

    Use blitz

--- src/scapin_demo.cpp
+++ src/scapin_demo.cpp
@@ -15,7 +15,9 @@
 #include "blitz/array.h"
 
 using complex128 = std::complex<double>;
-using Scalar = std::complex<double>;
+
+double squared_modulus(complex128 x) { return std::norm(x); }
+BZ_DECLARE_FUNCTION(squared_modulus)
 
 template <typename T, size_t N>
 std::array<T, N> array_strides(const std::array<T, N> size) {
@@ -53,28 +55,25 @@ std::string repr_array_2D(const std::array<size_t, 2> size,
   return stream.str();
 }
 
-template <typename T>
-void nninterp(const std::array<size_t, 2> isize,
-              const std::array<size_t, 2> istride, const T *in,
-              std::array<size_t, 2> osize, std::array<size_t, 2> ostride,
-              T *out) {
+template <typename T, int RANK>
+void nninterp(const blitz::Array<T, RANK> &in, blitz::Array<T, RANK> &out) {
   /*
    * i0, i1: indices in output array
    * i: offset in output array
    * j0, j1: indices in input array
    * j: offset in input array
    */
-  std::array<size_t, 2> r;
-  for (size_t k = 0; k < 2; ++k) {
-    r[k] = osize[k] / isize[k];
-  }
-  for (size_t i0 = 0; i0 < osize[0]; ++i0) {
-    size_t j0 = i0 / r[0];
-    size_t i = ostride[0] * i0;
-    for (size_t i1 = 0; i1 < osize[1]; ++i1, i += ostride[1]) {
-      size_t j1 = i1 / r[1];
-      size_t j = istride[0] * j0 + istride[1] * j1;
-      out[i] = in[j];
+  auto ishape = in.shape();
+  auto oshape = out.shape();
+  //  blitz::TinyVector<int, RANK> r;
+  //  for (int i = 0; i < RANK; i++) r[i] = oshape[i] / ishape[i];
+  auto r = out.shape() / in.shape();
+  // TODO: this loop is not dimension independent
+  for (int i0 = 0; i0 < oshape[0]; ++i0) {
+    int j0 = i0 / r[0];
+    for (int i1 = 0; i1 < oshape[1]; ++i1) {
+      int j1 = i1 / r[1];
+      out(i0, i1) = in(j0, j1);
     }
   }
 }
@@ -97,124 +96,134 @@ class FFTWComplexBuffer {
   FFTWComplexBuffer &operator=(const FFTWComplexBuffer &&) = delete;
 };
 
+template <size_t RANK>
+blitz::Array<complex128, RANK> create_array(
+    blitz::TinyVector<int, RANK> shape) {
+  auto size = blitz::product(shape);
+  auto c_data = fftw_malloc(size * sizeof(fftw_complex));
+  auto cpp_data = reinterpret_cast<complex128 *>(c_data);
+  return blitz::Array<complex128, RANK>(cpp_data, shape,
+                                        blitz::neverDeleteData);
+  // TODO: Huge memory leak!
+}
+
 template <typename GREENC>
-void run(GREENC gamma, std::array<size_t, GREENC::dim> Nc,
-         std::array<size_t, GREENC::dim> Nf, Scalar *eta_f) {
-  std::array<double, gamma.dim> L;
-  L.fill(1.);
+void run(GREENC gamma, blitz::TinyVector<int, GREENC::dim> Nc,
+         blitz::TinyVector<int, GREENC::dim> Nf,
+         blitz::Array<complex128, GREENC::dim + 1> &eta_f) {
+  blitz::TinyVector<double, GREENC::dim> L;
+  L = 1.;
 
   /*
    * tau_in: value of the prescribed polarization inside the patch
    * tau_out: value of the prescribed polarization outside the patch
    */
-  std::array<double, gamma.dim> patch_ratio;
-  patch_ratio.fill(0.125);
-  std::array<size_t, gamma.dim> patch_size;
-  for (size_t k = 0; k < gamma.dim; ++k) {
-    patch_size[k] = size_t(std::round(patch_ratio[k] * Nc[k]));
+  blitz::TinyVector<double, GREENC::dim> patch_ratio;
+  patch_ratio = 0.125;
+  blitz::TinyVector<int, GREENC::dim> patch_size;
+  for (int i = 0; i < GREENC::dim; i++) {
+    patch_size[i] = int(std::round(patch_ratio[i] * Nc[i]));
   }
 
-  std::array<double, gamma.isize> tau_in, tau_out;
-  tau_in.fill(0.);
+  blitz::TinyVector<double, GREENC::isize> tau_in, tau_out;
+  tau_in = 0.;
   tau_in[gamma.isize - 1] = 1.;
-  tau_out.fill(0.);
+  tau_out = 0.;
 
-  FFTWComplexBuffer tau{array_num_cells(Nc) * gamma.isize};
+  blitz::TinyVector<int, GREENC::dim + 1> tau_shape;
+  for (int i = 0; i < gamma.dim; i++) tau_shape[i] = Nc[i];
+  tau_shape[GREENC::dim] = GREENC::isize;
+  auto tau = create_array(tau_shape);
+
   // TODO: This is not dimension independent
-  for (size_t i0 = 0, i = 0; i0 < Nc[0]; ++i0) {
-    for (size_t i1 = 0; i1 < Nc[1]; ++i1, i += gamma.isize) {
+  for (int i0 = 0, i = 0; i0 < Nc[0]; ++i0) {
+    for (int i1 = 0; i1 < Nc[1]; ++i1, i += gamma.isize) {
       bool in = (i0 < patch_size[0]) && (i1 < patch_size[1]);
-      for (size_t k = 0; k < gamma.isize; ++k) {
-        tau.cpp_data[i + k] = in ? tau_in[k] : tau_out[k];
+      for (int k = 0; k < gamma.isize; ++k) {
+        tau(i0, i1, k) = in ? tau_in[k] : tau_out[k];
       }
     }
   }
 
-  // Set tau to the DFT of the polarization
-  std::array<int, gamma.dim> n;
-
-  // TODO: this ugly conversion from size_t to int is needed
-  for (size_t k = 0; k < gamma.dim; ++k) {
-    n[k] = int(Nc[k]);
-  }
-  auto p = fftw_plan_many_dft(gamma.dim, n.data(), gamma.isize, tau.c_data,
-                              nullptr, gamma.isize, 1, tau.c_data, nullptr,
-                              gamma.isize, 1, FFTW_FORWARD, FFTW_ESTIMATE);
+  auto tau_data = reinterpret_cast<fftw_complex *>(tau.data());
+  auto p = fftw_plan_many_dft(GREENC::dim, Nc.data(), GREENC::isize, tau_data,
+                              nullptr, GREENC::isize, 1, tau_data, nullptr,
+                              GREENC::isize, 1, FFTW_FORWARD, FFTW_ESTIMATE);
   fftw_execute(p);
   fftw_destroy_plan(p);
 
   // Set eta to the DFT of gamma_h(tau)
-  MoulinecSuquet94<decltype(gamma)> gamma_h{gamma, Nc, L};
-  FFTWComplexBuffer eta{array_num_cells(Nc) * gamma.osize};
+  // TODO: this is ugly, see Issue #3
+  std::array<size_t, GREENC::dim> Nc_;
+  std::array<double, GREENC::dim> L_;
+  for (int i = 0; i < GREENC::dim; i++) {
+    Nc_[i] = size_t(Nc[i]);
+    L_[i] = L[i];
+  }
+  MoulinecSuquet94<decltype(gamma)> gamma_h{gamma, Nc_, L_};
+  auto eta_shape{tau_shape};
+  eta_shape[GREENC::dim] = GREENC::osize;
+  auto eta = create_array(eta_shape);
+  const auto all = blitz::Range::all();
   // TODO: this is not dimension independent
-  for (size_t i0 = 0, i = 0, j = 0; i0 < Nc[0]; ++i0) {
-    for (size_t i1 = 0; i1 < Nc[1]; ++i1, i += gamma.isize, j += gamma.osize) {
-      size_t n[gamma.dim] = {i0, i1};
-      gamma_h.apply(n, tau.cpp_data + i, eta.cpp_data + j);
+  for (int i0 = 0; i0 < Nc[0]; ++i0) {
+    for (int i1 = 0; i1 < Nc[1]; ++i1) {
+      size_t n[GREENC::dim] = {i0, i1};
+      gamma_h.apply(n, tau(i0, i1, all).data(), eta(i0, i1, all).data());
     }
   }
 
-  // Handle the null-frequency case
-  for (size_t k = 0; k < gamma.osize; k++) {
-    eta.cpp_data[k] = 0.;
-  }
+  eta(0, 0, all) = 0.;
 
   // Set eta to gamma_h(tau)
-  p = fftw_plan_many_dft(gamma.dim, n.data(), gamma.osize, eta.c_data, nullptr,
-                         gamma.osize, 1, eta.c_data, nullptr, gamma.osize, 1,
-                         FFTW_BACKWARD, FFTW_ESTIMATE);
+  auto eta_data = reinterpret_cast<fftw_complex *>(eta.data());
+  p = fftw_plan_many_dft(GREENC::dim, Nc.data(), GREENC::osize, eta_data,
+                         nullptr, GREENC::osize, 1, eta_data, nullptr,
+                         GREENC::osize, 1, FFTW_BACKWARD, FFTW_ESTIMATE);
   fftw_execute(p);
   fftw_destroy_plan(p);
 
   // Normalize inverse DFT
-  const double normalization = 1. / array_num_cells(Nc);
-  for (size_t k = 0; k < eta.size; ++k) {
-    eta.cpp_data[k] *= normalization;
-  }
+  const double normalization = 1. / blitz::product(Nc);
+  eta *= normalization;
 
-  // Refine eta
-  std::array<size_t, gamma.dim> istrides, ostrides;
-  istrides[gamma.dim - 1] = gamma.osize;
-  ostrides[gamma.dim - 1] = gamma.osize;
-  for (int k = gamma.dim - 2; k >= 0; k--) {
-    istrides[k] = istrides[k + 1] * Nc[k + 1];
-    ostrides[k] = ostrides[k + 1] * Nf[k + 1];
-  }
+  // TODO This is not dimension independent
   for (size_t k = 0; k < gamma.osize; k++) {
-    nninterp(Nc, istrides, eta.cpp_data + k, Nf, ostrides, eta_f + k);
+    nninterp(eta(all, all, k), eta_f(all, all, k));
   }
 }
 
 int main() {
   const size_t dim = 2;
-  Hooke<Scalar, dim> gamma{1.0, 0.3};
+  Hooke<complex128, dim> gamma{1.0, 0.3};
   const size_t num_refinements = 6;
-  std::array<size_t, num_refinements> N;
+  std::array<int, num_refinements> N;
   N[0] = 8;
   for (size_t k = 1; k < num_refinements; ++k) {
     N[k] = 2 * N[k - 1];
   }
   std::cout << repr(N) << std::endl;
 
-  std::array<size_t, dim> Nc, Nf;
-  Nf.fill(N[num_refinements - 1]);
-  size_t num_cells = array_num_cells(Nf);
+  blitz::TinyVector<int, dim> Nc, Nf;
+  Nf = N[num_refinements - 1];
+  auto num_cells = blitz::product(Nf);
 
-  Scalar *results[num_refinements];
+  blitz::TinyVector<int, dim + 1> eta_shape;
+  for (int i = 0; i < dim; i++) eta_shape[i] = Nf[i];
+  eta_shape[dim] = gamma.osize;
+  blitz::Array<complex128, dim + 1> results[num_refinements];
   for (size_t r = 0; r < num_refinements; r++) {
-    Nc.fill(N[r]);
-    auto eta = new Scalar[num_cells * gamma.osize];
+    Nc = N[r];
+    blitz::Array<complex128 , dim+1> eta{eta_shape};
     run(gamma, Nc, Nf, eta);
-    results[r] = eta;
+    results[r].reference(eta);
   }
 
   auto eta_ref = results[num_refinements - 1];
   for (size_t r = 0; r < num_refinements; ++r) {
     auto eta = results[r];
-    double norm = 0.;
-    for (size_t i = 0; i < num_cells * gamma.osize; ++i) {
-      norm += std::norm(eta[i] - eta_ref[i]);
-    }
-    std::cout << N[r] << ", " << norm << std::endl;
+    auto eps = eta - eta_ref;
+    complex128 norm2 = blitz::sum(eps * blitz::conj(eps));
+    std::cout << N[r] << ", " << norm2 << std::endl;
   }
 }
