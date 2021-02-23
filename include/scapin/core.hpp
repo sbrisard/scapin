c951ad0eadf4a232cf99c66d7768175e49f7e11c
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-02-23 14:29:34 +0100

    New type trait: std::complex<T> ↦ T

--- include/scapin/core.hpp
+++ include/scapin/core.hpp
@@ -10,6 +10,19 @@ namespace scapin {
 template <int DIM>
 concept spatial_dimension = (DIM == 2) || (DIM == 3);
 
+template <class T>
+struct remove_complex {
+  typedef T type;
+};
+
+template <class T>
+struct remove_complex<std::complex<T>> {
+  typedef T type;
+};
+
+template <class T>
+using remove_complex_t = typename remove_complex<T>::type;
+
 template <typename Iterator>
 std::string repr(Iterator first, Iterator last) {
   std::ostringstream stream;
