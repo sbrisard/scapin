f917923dd5e00240be02c6178016483f97e12455
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-02-23 16:29:54 +0100

    Simplify tests

--- tests/test_scapin.cpp
+++ tests/test_scapin.cpp
@@ -1,152 +1,133 @@
-#define _USE_MATH_DEFINES
-
 #include <array>
-#include <cmath>
-#include <iostream>
+#include <numbers>
+#include <stdexcept>
 
 #include "catch2/catch.hpp"
 #include "scapin/hooke.hpp"
 
-template <size_t DIM>
-void test_data() {
-  const double mu = 10.;
-  const double nu = 0.3;
+template <int DIM>
+inline constexpr int sym = (DIM * (DIM + 1)) / 2;
 
-  scapin::Hooke<double, DIM> hooke{mu, nu};
-  REQUIRE(hooke.isize == (DIM * (DIM + 1)) / 2);
-  REQUIRE(hooke.osize == hooke.osize);
-  REQUIRE(hooke.mu == mu);
-  REQUIRE(hooke.nu == nu);
+template <int DIM>
+std::pair<int, int> unravel_index(int ij);
+
+template <>
+std::pair<int, int> unravel_index<2>(int ij) {
+  switch (ij) {
+    case 0:
+      return std::make_pair(0, 0);
+    case 1:
+      return std::make_pair(1, 1);
+    case 2:
+      return std::make_pair(0, 1);
+    default:
+      throw std::invalid_argument("unexpected value");
+  }
 }
 
-template <size_t DIM>
-constexpr auto _ij2i() {
-  if constexpr (DIM == 2) return std::array<size_t, 3>{0, 1, 0};
-  if constexpr (DIM == 3) return std::array<size_t, 6>{0, 1, 2, 1, 2, 0};
+template <>
+std::pair<int, int> unravel_index<3>(int ij) {
+  switch (ij) {
+    case 0:
+      return std::make_pair(0, 0);
+    case 1:
+      return std::make_pair(1, 1);
+    case 2:
+      return std::make_pair(2, 2);
+    case 3:
+      return std::make_pair(1, 2);
+    case 4:
+      return std::make_pair(2, 0);
+    case 5:
+      return std::make_pair(0, 1);
+    default:
+      throw std::invalid_argument("unexpected argument");
+  }
 }
 
-template <size_t DIM>
-constexpr auto _ij2j() {
-  if constexpr (DIM == 2) return std::array<size_t, 3>{0, 1, 1};
-  if constexpr (DIM == 3) return std::array<size_t, 6>{0, 1, 2, 2, 0, 1};
-}
-
-template <size_t DIM>
-void green_operator_matrix(double *n, double nu, double *out) {
-  constexpr size_t sym = (DIM * (DIM + 1)) / 2;
-
-  auto ij2i = _ij2i<DIM>();
-  auto ij2j = _ij2j<DIM>();
-
-  double *gamma_ijkl = out;
-
-  for (size_t ij = 0; ij < sym; ij++) {
-    size_t const i = ij2i[ij];
-    size_t const j = ij2j[ij];
-    double const w_ij = ij < DIM ? 1. : M_SQRT2;
-    for (size_t kl = 0; kl < sym; kl++, gamma_ijkl++) {
-      size_t const k = ij2i[kl];
-      size_t const l = ij2j[kl];
-      double const w_kl = kl < DIM ? 1. : M_SQRT2;
+template <int DIM>
+void green_operator_matrix(std::array<double, DIM> const &n, double nu,
+                           std::array<double, sym<DIM> * sym<DIM>> &out) {
+  int ijkl = 0;
+  for (int ij = 0; ij < sym<DIM>; ij++) {
+    auto const [i, j] = unravel_index<DIM>(ij);
+    double const w_ij = ij < DIM ? 1. : std::numbers::sqrt2;
+    for (int kl = 0; kl < sym<DIM>; kl++, ijkl++) {
+      auto const [k, l] = unravel_index<DIM>(kl);
+      double const w_kl = kl < DIM ? 1. : std::numbers::sqrt2;
       double const delta_ik = i == k ? 1. : 0.;
       double const delta_il = i == l ? 1. : 0.;
       double const delta_jk = j == k ? 1. : 0.;
       double const delta_jl = j == l ? 1. : 0.;
-      gamma_ijkl[0] =
-          w_ij * w_kl *
-          (0.25 * (delta_ik * n[j] * n[l] + delta_il * n[j] * n[k] +
-                   delta_jk * n[i] * n[l] + delta_jl * n[i] * n[k]) -
-           0.5 * n[i] * n[j] * n[k] * n[l] / (1. - nu));
+      out[ijkl] = w_ij * w_kl *
+                  (0.25 * (delta_ik * n[j] * n[l] + delta_il * n[j] * n[k] +
+                           delta_jk * n[i] * n[l] + delta_jl * n[i] * n[k]) -
+                   0.5 * n[i] * n[j] * n[k] * n[l] / (1. - nu));
     }
   }
 }
 
-template <size_t DIM>
-class DirectionGenerator {
- private:
-  size_t const num_theta;
-  size_t const num_phi;
-  double const delta_theta;
-  double const delta_phi;
+std::vector<std::array<double, 2>> gen_directions(int num_theta) {
+  double const delta_theta = std::numbers::pi / (num_theta - 1.);
+  std::vector<std::array<double, 2>> directions;
+  for (int i = 0; i < num_theta; i++) {
+    double theta = i * delta_theta;
+    directions.push_back({cos(theta), sin(theta)});
+  }
+  return directions;
+}
 
- public:
-  DirectionGenerator(size_t num_theta = 1, size_t num_phi = 1)
-      : num_theta(num_theta),
-        num_phi(num_phi),
-        delta_theta(M_PI / (num_theta - 1.)),
-        delta_phi(2 * M_PI / (double)num_phi) {
-    if ((DIM == 2) && (num_phi != 1)) {
-      throw std::invalid_argument("num_phi must be equal to 1 when DIM = 2");
+std::vector<std::array<double, 3>> gen_directions(int num_theta, int num_phi) {
+  double const delta_theta = std::numbers::pi / (num_theta - 1.);
+  double const delta_phi = 2 * std::numbers::pi / double(num_phi);
+  std::vector<std::array<double, 3>> directions{};
+  for (int i = 0; i < num_theta; i++) {
+    double const theta = i * delta_theta;
+    double const cos_theta = cos(theta);
+    double const sin_theta = sin(theta);
+    for (int j = 0; j < num_phi; j++) {
+      double const phi = j * delta_phi;
+      directions.push_back(
+          {sin_theta * cos(phi), sin_theta * sin(phi), cos_theta});
     }
   }
+  return directions;
+}
 
-  std::array<double, DIM> as_array(size_t i) const {
-    if constexpr (DIM == 2) {
-      double const theta = delta_theta * i;
-      return {cos(theta), sin(theta)};
-    }
-    if constexpr (DIM == 3) {
-      double const theta = delta_theta * (i / num_phi);
-      double const phi = delta_phi * (i % num_phi);
-      double const sin_theta = sin(theta);
-      return {sin_theta * cos(phi), sin_theta * sin(phi), cos(theta)};
-    }
-  }
-
-  class DirectionIterator {
-   private:
-    size_t current;
-    const DirectionGenerator &generator;
-
-   public:
-    DirectionIterator(const DirectionGenerator &generator, size_t current)
-        : generator(generator), current(current){};
-
-    std::array<double, DIM> operator*() { return generator.as_array(current); };
-    bool operator!=(const DirectionIterator &j) const {
-      return current != j.current;
-    }
-    void operator++() { ++current; }
-  };
-
-  DirectionIterator begin() const { return DirectionIterator(*this, 0); }
-  DirectionIterator end() const {
-    return DirectionIterator(*this, num_theta * num_phi);
-  }
-};
-
-template <size_t DIM>
-void test_grop_hooke_apply() {
-  constexpr size_t const sym = (DIM * (DIM + 1)) / 2;
+template <int DIM>
+void test_hooke_apply() {
   double const mu = 1.0;
   double const nu = 0.3;
 
-  std::array<double, 3> k_norm = {1.2, 3.4, 5.6};
+  std::vector<double> norms{1.2, 3.4, 5.6};
 
   scapin::Hooke<double, DIM> gamma{mu, nu};
-  double exp[sym * sym];
-  double act[sym * sym];
-  double tau[sym] = {0.};
-  double eps[sym];
+  std::array<double, sym<DIM> * sym<DIM>> exp{};
+  std::array<double, sym<DIM> * sym<DIM>> act{};
+  std::array<double, sym<DIM>> tau{};
+  std::array<double, sym<DIM>> eps{};
 
-  size_t const num_theta = DIM == 2 ? 20 : 10;
-  size_t const num_phi = DIM == 2 ? 1 : 20;
-  for (auto n : DirectionGenerator<DIM>(num_theta, num_phi)) {
-    green_operator_matrix<DIM>(n.data(), nu, exp);
-    for (size_t i = 0; i < k_norm.size(); i++) {
-      double k_vec[DIM];
-      for (size_t j = 0; j < DIM; j++) {
-        k_vec[j] = k_norm[i] * n[j];
-      }
-      for (size_t col = 0; col < sym; col++) {
+  std::vector<std::array<double, DIM>> directions;
+  if constexpr (DIM == 2) {
+    directions = gen_directions(20);
+  } else if constexpr (DIM == 3) {
+    directions = gen_directions(10, 20);
+  }
+  for (auto n : directions) {
+    green_operator_matrix<DIM>(n, nu, exp);
+    for (auto const norm : norms) {
+      std::array<double, DIM> k{};
+      std::transform(n.cbegin(), n.cend(), k.begin(),
+                     [norm](auto n_) { return norm * n_; });
+      for (int col = 0; col < sym<DIM>; col++) {
         tau[col] = 1.;
-        gamma.apply(k_vec, tau, eps);
-        for (size_t row = 0; row < sym; row++) {
-          act[col + sym * row] = eps[row];
+        gamma.apply(k.data(), tau.data(), eps.data());
+        for (int row = 0; row < sym<DIM>; row++) {
+          act[col + sym<DIM> * row] = eps[row];
         }
         tau[col] = 0.;
       }
-      for (size_t ijkl = 0; ijkl < sym * sym; ijkl++) {
+      for (int ijkl = 0; ijkl < sym<DIM> * sym<DIM>; ijkl++) {
         REQUIRE(act[ijkl] == Approx(exp[ijkl]).epsilon(1e-12).margin(1e-12));
       }
     }
@@ -155,13 +136,9 @@ void test_grop_hooke_apply() {
 
 TEST_CASE("Continuous Green operator") {
   SECTION("Hooke model") {
-    SECTION("Data") {
-      SECTION("2D") { test_data<2>(); }
-      SECTION("3D") { test_data<3>(); }
-    }
     SECTION("Apply") {
-      SECTION("2D") { test_grop_hooke_apply<2>(); }
-      SECTION("3D") { test_grop_hooke_apply<3>(); }
+      SECTION("2D") { test_hooke_apply<2>(); }
+      SECTION("3D") { test_hooke_apply<3>(); }
     }
   }
 }
